8d56bbec200704f48ddaf19d385cfd167e50df84
Sarah Ogunko <sarahogunko@hotmail.com>
2025-03-30 13:45:22 +0100

    filtered first ten restaurants

--- src/Components/Restaurantlist.js
+++ src/Components/Restaurantlist.js
@@ -1,11 +1,19 @@
+import React from "react";
 import { Restaurant } from "./Restaurant";
 
-export const Restaurantlist = () =>{
-  return (
-    <div>
-       <h2>Restaunts in the area</h2>
-       <Restaurant/>
-
-    </div>
-  );
+export const Restaurantlist = ({data}) =>{
+    if (!data) {
+        return <p>Loading...</p>;
+      }
+    
+      const restaurantData = data.restaurants.slice(0, 10); 
+    
+      return (
+        <div>
+          {restaurantData.map((restaurant) => (
+            <Restaurant key={restaurant.id} restaurant={restaurant} />
+          ))}
+        </div>
+      );
+    
 }

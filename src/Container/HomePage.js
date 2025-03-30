2da278adc6d2674837219c6344ece1e557107938
Sarah Ogunko <sarahogunko@hotmail.com>
2025-03-31 00:23:51 +0100

    succesfully fectched data from api

--- src/Container/HomePage.js
+++ src/Container/HomePage.js
@@ -1,11 +1,27 @@
 import { Restaurantlist } from "../Components/Restaurantlist";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export const Homepage = () =>{
-  return (
-    <div>
-       <h2>Restaunts</h2>
-       <Restaurantlist/>
+    const [data, setData] = useState(null);
+  
+    const fetchData = async () => {
+        const response = await axios.get(
+          "https://thingproxy.freeboard.io/fetch/https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
+        );
+        setData(response.data);
+    };
+  
+    useEffect(() => {
+      fetchData();
+      console.log(data)
+    }, []);
+  
+    return (
+      <div className="App">
+        <h1>Restaurants Near EC4M7RF</h1>
+       <Restaurantlist data = {data}/>
+      </div>
+    );
+  }
 
-    </div>
-  );
-}

da4de3821dc0db99d292b8f6621adb65c7335e21
Sarah Ogunko <sarahogunko@hotmail.com>
2025-03-31 19:07:13 +0100

    refactored restaurantlist

--- src/Components/Restaurantlist.js
+++ src/Components/Restaurantlist.js
@@ -2,22 +2,17 @@ import React from "react";
 import { Restaurant } from "./Restaurant";
 
 export const Restaurantlist = ({ data }) => {
-  if (!data) {
-    return (
-      <div className="spinner-container">
-        <div className="spinner"></div>
-      </div>
-    );
-  }
-
-  const restaurantData = data.restaurants.slice(0, 10);
-
-  return (
+  return !data ? (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  ) : (
     <div className="Restaurant-list">
-      {restaurantData.map((restaurant) => (
+      {data.restaurants.slice(0, 10).map((restaurant) => (
         <Restaurant key={restaurant.id} restaurant={restaurant} />
       ))}
     </div>
   );
 };
+
 
